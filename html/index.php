a86b25c431b7f31f20c69df88de6b83f128d9b6a
Max <max@olibri.us>
2024-12-05 12:09:43 +0100

    example cours

--- html/index.php
+++ html/index.php
@@ -7,6 +7,19 @@
 <h2>
   <?php echo 'Salut la classe M1 Dev de l\'ECV !' ?>
 </h2>
+<h4>Nous sommes le <?php echo date('d/m/Y') ?> et il est <? echo date('h:i') ?></h4>
+
+<p>
+  <? $name = 'Maxime' ?>
+  <? echo "Vous vous appelez {$name} !" ?>
+</p>
+
+<p>
+  <pre>
+  <?php var_dump($_ENV) ?>
+  </pre>
+</p>
+
 </body>
 
 </html>
