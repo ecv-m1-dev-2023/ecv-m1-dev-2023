bdf8fb3541fbd1d2c0c703d7aaf5ce8f98b29ad7
Guillaume Lamourec <parkerpool@live.fr>
2024-12-05 12:30:02 +0100

    début du layout

--- html/index.php
+++ html/index.php
@@ -1,25 +1,24 @@
 <html lang="fr">
 
 <head>
+  <link href="style.css" rel="stylesheet"/>
 </head>
 
 <body>
-<h2>
-  <?php echo 'Salut la classe M1 Dev de l\'ECV !' ?>
-</h2>
-<h4>Nous sommes le <?php echo date('d/m/Y') ?> et il est <? echo date('h:i') ?></h4>
-
-<p>
-  <? $name = 'Maxime' ?>
-  <? echo "Vous vous appelez {$name} !" ?>
-</p>
-
-<p>
-  <pre>
-  <?php var_dump($_ENV) ?>
-  </pre>
-</p>
-
+  <?php 
+    $title = "Les chats c'est cool";
+  ?>
+<section>
+  <article>
+    <div>
+      <h1 class="title"><?php echo $title ?></h1>
+      <span><time>3 days ago</time></span>
+      <span>Maxime</span>
+    </div>
+    <img src="https://www.assuropoil.fr/wp-content/uploads/2023/07/avoir-un-chat-sante.jpg" alt="chat" class="image">
+    <p class="content">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem iure ut placeat eveniet quos nulla provident ducimus totam quod iste repudiandae sequi corrupti quia, est atque rem repellat itaque delectus ab iusto. Inventore, cupiditate magni cum est sit delectus ratione maxime dignissimos eos itaque commodi dolore, harum ad fugit non unde ullam! Placeat, accusantium aliquam ab ea quasi tenetur praesentium mollitia accusamus magni repellat totam quam quos illo iusto ratione sint nostrum ipsa. Eligendi praesentium possimus voluptatem illo quidem! Quibusdam temporibus, mollitia veritatis architecto alias ipsam delectus placeat, quia libero hic quasi exercitationem omnis fuga saepe odit! Aperiam architecto perspiciatis placeat numquam voluptates ullam deserunt quas omnis. Placeat aperiam eaque possimus ducimus fuga voluptatem iure? A quisquam non rem nobis hic odit. Accusamus deleniti provident porro aspernatur nulla adipisci, quod in nam similique sint. Consequuntur dolor est maxime in mollitia rem quod aliquid facere dolorem! Error assumenda eum quidem, eaque expedita rerum accusantium commodi eius aspernatur nihil doloribus laborum magni. Architecto facilis pariatur aut temporibus corporis ratione magni commodi at eius totam, minus excepturi delectus voluptatum sunt debitis molestiae inventore dolores soluta? Harum, omnis? Sed, necessitatibus molestias, asperiores totam veniam odio voluptatem eaque magnam tempora quos libero rerum tenetur nam.</p>
+  </article>
+</section>
 </body>
 
 </html>
