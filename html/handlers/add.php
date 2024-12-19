e00fa2ec670059ceccf764c59868bdfa7402025d
Samuel-Labagnere <samuel.labagnere@gmail.com>
2024-12-19 09:54:40 +0100

    Saving articles in session

--- html/handlers/add.php
+++ html/handlers/add.php
@@ -1,5 +1,19 @@
 <?php
 
-foreach ($_POST as $key => $value) {
-    echo $key . " : " . $value . "<br>";
-}
+session_start();
+
+$_SESSION['articles'] = $_SESSION['articles'] ?? [];
+$articles = $_SESSION['articles']; // safe handler
+
+$articles[] = [
+    'id' => uniqid(),
+    'title' => $_POST['title'],
+    'date' => date('d/m/Y'),
+    'author' => $_POST['author'],
+    'img' => "https://www.assuropoil.fr/wp-content/uploads/2023/07/avoir-un-chat-sante.jpg",
+    'content' => $_POST['content'],
+    'comments' => []
+];
+
+$_SESSION['articles'] = $articles; // save
+header('Location: /index.php');
--- html/index.php
+++ html/index.php
@@ -9,11 +9,15 @@
   include_once("./data.php");
   ?>
   <section>
-    <?php foreach ($articles as $article): ?>
-      <article>
-        <?php include 'parts/article.php'; ?>
-      </article>
-    <?php endforeach; ?>
+    <?php if ($articles) : ?>
+      <?php foreach ($articles as $article): ?>
+        <article>
+          <?php include 'parts/article.php'; ?>
+        </article>
+      <?php endforeach; ?>
+    <?php else : ?>
+      <h3>Oops! Aucun article. :)</h3>
+    <?php endif; ?>
     <aside>
       <?php include 'forms/article-form.php'; ?>
     </aside>
