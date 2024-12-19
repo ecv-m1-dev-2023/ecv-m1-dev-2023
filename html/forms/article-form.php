492ac04f6b1e04b47ce930e7bf89c2f88f6129b7
Gabriel BREHAULT <gabrielbrehault@MacBook-Pro-de-Gabriel.local>
2024-12-19 10:31:56 +0100

    gestion de l'envoue de fichier

--- html/forms/article-form.php
+++ html/forms/article-form.php
@@ -1,11 +1,11 @@
-<form action="/handlers/add.php" method="POST" class="form">
+<form action="/handlers/add.php" method="POST" class="form" enctype="multipart/form-data">
   <h3>Ajouter un article </h3>
   <label for="title"> Titre</label>
   <input type="text" name="title">
   <label for="author"> Auteur</label>
   <input type="text" name="author">
-  <label for="image"> Image</label>
-  <input type="file" name="image">
+  <label for="article_img"> Image</label>
+  <input type="file" name="article_img">
   <label for="content"> Contenu</label>
   <textarea name="content"></textarea>
   <input type="submit" value="Enregistrer" />
--- html/handlers/add.php
+++ html/handlers/add.php
@@ -5,12 +5,20 @@ session_start();
 $_SESSION['articles'] = $_SESSION['articles'] ?? [];
 $articles = $_SESSION['articles']; // safe handler
 
+$filePath = $_FILES['article_img']['tmp_name'];
+$fileName = $_FILES['article_img']['name'];
+$newFileName = "/images/" . $fileName;
+
+if (isset($filePath)) {
+    move_uploaded_file($filePath, ".." . $newFileName);
+}
+
 $articles[] = [
     'id' => uniqid(),
     'title' => $_POST['title'],
     'date' => date('d/m/Y'),
     'author' => $_POST['author'],
-    'img' => "https://www.assuropoil.fr/wp-content/uploads/2023/07/avoir-un-chat-sante.jpg",
+    'img' => $newFileName,
     'content' => $_POST['content'],
     'comments' => []
 ];
